07bfef16d7c3f0cb3b72edd0d7d27f7005203a2d
Anav Chand <anavkki@gmail.com>
2024-11-12 16:42:44 +1200

    feat: setup flags and option select

---
+++ bin/kloc.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env Node
+
+import { klocOneline, klocFile, klocTable } from "../src/commands/index.js";
+import { ONELINE, FILE, TABLE } from "../src/utils/constants.js"
+import { select } from "@inquirer/prompts";
+import { program } from "commander";
+
+
+program
+    // CLI details
+    .version("1.0.0")
+    .name("Kloc")
+    .description("Count lines of code in current directory")
+
+    // Flag options
+    .option("-o, --oneline", "Display total lines of code")
+    .option("-f, --file", "Display lines of code for each file (tree format)")
+    .option("-t, --table", "Display lines of code for each language (table format)")
+
+    // Main logic
+    .action(async (options) => {
+        let klocType = 0
+        console.log("");
+
+        if (options.oneline) klocType = ONELINE
+
+        else if (options.file) klocType = FILE
+
+        else if (options.table) klocType = TABLE
+
+        // Fallback for no flags using select
+        else {
+            const answer = await select({
+                message: "How would you like to format the output?",
+                choices: [
+                    {
+                        name: "oneline",
+                        value: ONELINE,
+                    },
+                    {
+                        name: "file",
+                        value: FILE,
+                    },
+                    {
+                        name: "table",
+                        value: TABLE,
+                    },
+                ],
+            });
+
+            console.log("")
+            klocType = answer
+        }
+
+        switch (klocType) {
+            case ONELINE:
+                klocOneline()
+                break
+            case FILE:
+                klocFile()
+                break
+            case TABLE:
+                klocTable()
+                break
+        }
+
+        console.log("Built by https://anav.dev");
+    });
+
+program.parse(process.argv);
---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const ONELINE = 1
+export const FILE = 2
+export const TABLE = 3
