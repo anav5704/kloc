e13edafc239e6f35d81fc3093a6294585e1e4714
Anav Chand <anavkki@gmail.com>
2024-11-12 19:47:00 +1200

    feat: add kloc --oneline

--- src/commands/oneline.js
+++ src/commands/oneline.js
@@ -1,3 +1,30 @@
+import { readFileSync, readdirSync, statSync} from "fs"
+import { basename, join } from "path"
+
+const countKloc = (path, loc = 0) => {
+    const root = readdirSync(path)
+    
+    root.forEach((item) => {
+        if(item == "node_modules") return
+        if(item.startsWith(".")) return
+
+        const itemPath = join(path, item)
+
+        if(statSync(itemPath).isDirectory()) {
+            countKloc(itemPath, loc)
+        } 
+
+        else {
+            const file = readFileSync(itemPath, "utf8").toString()
+            loc += file.split("\n").length
+        }
+    })
+
+    return (loc / 1000).toPrecision(3)
+}
+
 export const klocOneline = () => {
-    console.log("oneline")
+    const koc = countKloc(process.cwd())
+    const name = basename(process.cwd())
+    console.log("Kloc for " + name, koc)
 }
